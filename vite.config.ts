22c3abbc4d90461b518f975ee873f49a3a299236
Withemmy1 <walletcenter1@gmail.com>
2024-11-11 20:24:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      external: [],
+    }
   },
   server: {
     proxy: {
